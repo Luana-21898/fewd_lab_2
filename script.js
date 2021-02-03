1cd8ebb4ed1f2e676c2c0160a2817b424f76954e
Luana-21898 <21898@student.dorset-college.ie>
2021-02-03 01:49:34 +0000

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+
+//Question 4
+function myAlphabetLength(){
+    console.log(myAlphabet.length)
+}
+myAlphabetLength();
+
+//Question 5
+const planets = ['Mercury', 'Venus', 'Mars', 'Saturn', 'Earth'];
+
+//Question 6
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+
+//Question 7
+const wowDatatypes = [2, 'Luana', true];
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
+
+//Question 11
+const hello = function() {
+    return 'Hello World!';
+};
+
+//Question 12
+console.log(hello());
